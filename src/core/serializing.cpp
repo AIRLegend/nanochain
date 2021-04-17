a88667be7a6d1c68da9c1ecde122060fdbb117be
AIRLegend <alvaroibrain@hotmail.com>
2021-04-17 12:26:37 +0200

    Add serialization capabilities

---
+++ src/core/serializing.cpp
@@ -0,0 +1,119 @@
+#include "core/serializing.h"
+
+#include <sstream>
+
+json transactionToJSON(const Transaction& tx)
+{
+    json repr = {
+            {"from", bytesToString(tx.m_from, SIGNATURE_LEN)},
+            {"to", bytesToString(tx.m_to, SIGNATURE_LEN)},
+            {"amount", tx.getAmount()},
+            {"signature", bytesToString(tx.m_signature, SIGNATURE_LEN)},
+    };
+
+    return repr;
+}
+
+json blockToJSON(const Block& block)
+{
+    std::string  prevHash = bytesToString(block.prev_hash);
+    std::string  blockHash = bytesToString(block.b_hash);
+
+    auto txs = json::array();
+
+    for (Transaction tx:block.txs)
+        txs.push_back(transactionToJSON(tx));
+
+    json repr = {
+            {"block_hash", blockHash},
+            {"prev_hash", prevHash},
+            {"txs", txs}
+    };
+
+    return repr;
+}
+
+json blocksToJSON(const std::vector<Block> &blocks)
+{
+    std::vector<json> reprs;
+
+    for (Block bl: blocks) {
+        reprs.push_back(blockToJSON(bl));
+    }
+
+    return reprs;
+}
+
+
+// Recovering from JSON format
+Transaction transactionFromJSON(const json& obj)
+{
+    std::string from_str = obj["from"];
+    std::string to_str = obj["to"];
+    std::string signature_str = obj["signature"];
+    int amount = obj["amount"];
+
+    unsigned char from[SIGNATURE_LEN];
+    unsigned char to[SIGNATURE_LEN];
+    stringToBytes(from_str, from);
+    stringToBytes(to_str, to);
+
+
+    Transaction tx(from, to, amount=amount);
+    stringToBytes(signature_str, tx.m_signature);
+    return tx;
+}
+
+Block blockFromJSON(const json& obj)
+{
+    std::string bhash = obj["block_hash"];
+    std::string phash = obj["prev_hash"];
+
+    Block b;
+
+    stringToBytes(phash, b.prev_hash);
+    stringToBytes(bhash, b.b_hash);
+
+    for (json json_t:obj["txs"]) {
+        b.txs.push_back(transactionFromJSON(json_t));
+    }
+
+    return b;
+}
+
+
+// IO
+void toDisk(const std::string& path, const json& obj)
+{
+    std::ofstream o(path);
+    o << std::setw(4) << obj << std::endl;
+}
+
+json fromDisk(const std::string& path)
+{
+    std::ifstream i(path);
+    json j;
+    i >> j;
+    return j;
+}
+
+
+// Utils for parsing byte arrays (hashes and signatures) to strings
+std::string bytesToString(const unsigned char* bytes, size_t len)
+{
+    std::stringstream hashStr;
+    for (int i=0; i<len; i++) {
+        hashStr << std::hex << std::setw(2) << std::setfill('0') << static_cast<unsigned int>(bytes[i]);
+    }
+    return hashStr.str();
+}
+
+void stringToBytes(const std::string& hash_str, unsigned char* out)
+{
+    for (int i=0; i<hash_str.size(); i+=2) {
+        std::istringstream converter(hash_str.substr(i, 2));
+        unsigned int value;
+        converter >>  std::hex >> value;
+        out[int(i/2)] = static_cast<unsigned char>(value);
+    }
+}
---
+++ src/core/serializing.h
@@ -0,0 +1,28 @@
+#ifndef NANOCHAIN_SERIALIZING_H
+#define NANOCHAIN_SERIALIZING_H
+
+#include "json.hpp"
+
+#include "core/transaction.h"
+#include "core/block.h"
+
+using json = nlohmann::json;
+
+// Parsing to JSON format
+json transactionToJSON(const Transaction& tx);
+json blockToJSON(const Block& block);
+json blocksToJSON(const std::vector<Block> &blocks);
+
+// Recovering from JSON format
+Transaction transactionFromJSON(const json& obj);
+Block blockFromJSON(const json& obj);
+
+// IO
+void toDisk(const std::string& path, const json& obj);
+json fromDisk(const std::string& path);
+
+// Utils for parsing byte arrays (hashes and signatures) to strings
+std::string bytesToString(const unsigned char* bytes, size_t len=HASH_SIZE);
+void stringToBytes(const std::string& hash_str, unsigned char* out);
+
+#endif //NANOCHAIN_SERIALIZING_H
