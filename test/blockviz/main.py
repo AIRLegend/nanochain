b448893e7ba51e60f0625ae29f92fe90e8742d92
AIRLegend <alvaroibrain@hotmail.com>
2021-05-25 19:39:58 +0200

    Get mempool state

--- test/blockviz/main.py
+++ test/blockviz/main.py
@@ -19,8 +19,21 @@ def get_blocks():
     res = res.decode("utf-8")
     return json.loads(res)["data"]
 
+def get_mempool():
+    client = Client(endpoint="tcp://localhost:5557")
+    payload = {"OP": 6, "payload": ""}
+    res = client.request(payload)
+    res = res.decode("utf-8")
+    return json.loads(res)["data"]
+
 
 @app.route('/')
 def root():
     r = get_blocks()
     return render_template('blocks.html', blocks=r)
+
+
+@app.route('/mempool')
+def mem():
+    r = get_mempool()
+    return render_template('mempool.html', transactions=r)
